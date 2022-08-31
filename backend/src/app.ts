81b1123cc9dd037bdf28f89054cb1ce1bb95eeb7
Marton <mdox@mdox.xyz>
2022-08-31 22:28:37 +0200

    Make backend and frontend production ready "serve".

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,10 +1,23 @@
 import express from "express";
+import { resolve } from "path";
 import driversRouter from "./routes/drivers/drivers.routes";
 
 const app = express();
 
 app.use(express.json());
-app.use("/static", express.static("database/static"));
+app.use("/static", express.static(resolve("database/static")));
 app.use("/api", driversRouter);
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(resolve("../frontend/build")));
+  app.get("*", (req, res) => {
+    if (req.path.endsWith(".html")) {
+      res.redirect(req.path.split(".").slice(0, -1).join("."));
+      return;
+    }
+
+    res.sendFile(resolve("../frontend/build/index.html"));
+  });
+}
 
 export default app;
--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,6 +1,6 @@
 import app from "./app";
 
-const PORT = 5000;
+const { PORT = 5000 } = process.env;
 
 app.listen(PORT, () => {
   console.log(`Server on: http://localhost:${PORT}`);
